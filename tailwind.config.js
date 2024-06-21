dea88f683a672181a58e089386d1604e67978cc5
AlfaizLuthfie <faizalafiluthfie@gmail.com>
2024-06-21 20:09:50 +0700

    Menambahkan Tampilan Landing Page

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        primary: '#5453FE',
+      },
+      fontFamily:{
+        poppins: ['Poppins', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
+
